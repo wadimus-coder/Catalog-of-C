a0534a2d90d3a6a1197e079c409bbb6e382292c5
Wadim Sagaidak <grusa1360@gmail.com>
2025-02-28 13:20:16 +0200

    function hello()

--- second-subfolder/app.c
+++ second-subfolder/app.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 #define F 2025
+
+void hello();
+
 int main()
 {
+    hello();
     printf("Hello Git in %d\n!", F);
 
     put("+++++++++++++++++");
     put("_________________\n");
 
     return 0;
+}
+
+void hello()
+{
+
+    printf("Hello Git :%d\n", F);
 }
